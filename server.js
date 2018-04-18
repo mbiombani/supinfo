9ff58bce5033eae6f4a768f2890df2123f861c20
stefen <mbiombanistefen@gmail.com>
2018-04-18 19:29:55 +0200

    ajout et creation de server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-//Import dependencies
+//Importation dependencies
 const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-//Connect to mongoDB server
+//Connection to mongoDB server
 mongoose.connect('mongodb://localhost/supinfo');
 mongoose.set('debug', true);
