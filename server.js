6c097d8186703271296ae548cb6085f850733599
stefen <mbiombanistefen@gmail.com>
2018-04-18 19:41:22 +0200

    ajout et creation du server node

--- server.js
+++ server.js
@@ -8,3 +8,28 @@ const mongoose = require('mongoose');
 //Connection to mongoDB server
 mongoose.connect('mongodb://localhost/supinfo');
 mongoose.set('debug', true);
+
+//Init express
+const app = express();
+
+//Enable bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Enable CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
+//Get environment port or use 3000
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+//Create HTTP server.
+const server = http.createServer(app);
+
+//Listen on port
+server.listen(port, () => console.log(`API running on localhost:${port}`));
